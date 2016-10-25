ece91407ab95eb617c3be62895d0efe797d99c67
Matt <matthewrmcgeehan2@gmail.com>
2016-10-25 11:25:08 -0400

    Flappers

---
+++ Assets/Scripts/Explosion.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+function Start () {
+	Invoke("KillMe", 8);
+}
+
+function KillMe () {
+Destroy(gameObject);
+}
---
+++ Assets/Scripts/Ship.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+public var expl : GameObject;
+
+function Start () {
+
+}
+
+function onTriggerEnter2D (other : Collider2D)
+ {
+ var newPos : Vector3 = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, 0); 
+ Instantiate(expl, newPos, Quaternion.identity);
+ Destroy(gameObject);
+}
+
+
